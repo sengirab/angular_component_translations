e7a1a2a9f0bf4474c1d3353b0c412b2cfcf68b5a
benjamin <bbloot@dynasource.com>
2018-08-22 11:16:16 +0200

    in between

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ extern crate serde_derive;
 extern crate serde_json;
 
 use component::{AngularComponent, ComponentType};
+use regex::CaptureMatches;
 use regex::Regex;
 use regex::RegexSet;
 use std::cmp::Ordering;
@@ -22,10 +23,13 @@ mod utilities;
 mod route;
 
 lazy_static! {
-    static ref ROUTES: Regex = Regex::new(r"(?m)Routes\s?=\s?\[\s[^;]*;").unwrap();
-}
-lazy_static! {
+    static ref ROUTES: Regex = Regex::new(r"(?m)Routes\s?=\s?(\[\s[^;]*);").unwrap();
     static ref ROUTE: Regex = Regex::new(r"(?m)\{([^}]*)},?").unwrap();
+
+    static ref PATH: Regex = Regex::new(r#"(?m)\spath:\s['"`](.*)['"`]"#).unwrap();
+    static ref COMPONENT: Regex = Regex::new(r"(?m)[^,|^{]\scomponent:\s(.*)").unwrap();
+    static ref LOAD: Regex = Regex::new(r#"(?m)\sloadChildren:\s['"`].*/(.*)#.*['"`]"#).unwrap();
+    static ref CHILDREN: Regex = Regex::new(r"(?m)children:\s?\[(\s[^;]*)][^;]").unwrap();
 }
 
 fn main() {
@@ -73,23 +77,50 @@ fn main() {
 
 fn get_routes(component: &AngularComponent) {
     let content = component.open_ts();
-    let routes = ROUTES.captures_iter(&content)
-        .fold(String::new(), |res, item| item[0].to_string());
+    let routes = capture_group(ROUTES.captures_iter(&content));
 
     // Initially give entry route folder routes. Determine from here on.
-    setup_route_hierarchy(&routes)
+    setup_route_hierarchy(&routes);
 }
 
 fn setup_route_hierarchy(routes: &String) {
     for route in ROUTE.captures_iter(routes) {
+        println!("ROUTe, {:?}", route);
+        let group: &str = &route[1];
         // Match all different types in here
-        let set = RegexSet::new(&[
-            r#"(?m)\spath:\s['"`](.*)['"`]"#,
-            r"(?m)[^,|^{]\scomponent:\s(.*)",
-            r#"(?m)\sloadChildren:\s['"`].*/(.*)#.*['"`]"#,
-            r"(?m)children:\s?\[\s[^;]*]" // If children are found, go recursive.
-        ]).unwrap();
+        let set = RegexSet::new(&[PATH.as_str(), COMPONENT.as_str(), LOAD.as_str(), CHILDREN.as_str()]).unwrap();
+        let set: Vec<_> = set.matches(group).into_iter().collect();
 
-        println!("Going to get routes {:?}", route[1].to_string());
+        println!("SET, {:?}", set);
+        for item in set {
+            match item {
+                // Concat path.
+                0 => {
+                    let path = capture_group(PATH.captures_iter(group));
+                    println!("Got path {:?}", path);
+                }
+                // Search for components in components.
+                1 => {
+                    let path = capture_group(COMPONENT.captures_iter(group));
+                    println!("Got component {:?}", path);
+                }
+                // Find file that's being loaded and go recursive
+                2 => {
+                    let path = capture_group(LOAD.captures_iter(group));
+                    println!("Got loaded children {:?}", path);
+                }
+                // Go recursive with matches.
+                3 => {
+                    let path = capture_group(CHILDREN.captures_iter(group));
+                    println!("Got children {:?}", path);
+                }
+                _ => {}
+            }
+        }
     }
+}
+
+fn capture_group(captures: CaptureMatches) -> String {
+    captures
+        .fold(String::new(), |res, item| item[1].to_string())
 }
